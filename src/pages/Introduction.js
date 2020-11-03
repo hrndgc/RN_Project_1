09ce4f03e222b70ba421ded409dbb449c2033127
Harun <67702854+hrndgc@users.noreply.github.com>
2020-11-03 21:54:58 +0300

    Created folders and files

---
+++ src/pages/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const Introduction = (props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Introduction</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export { Introduction };
